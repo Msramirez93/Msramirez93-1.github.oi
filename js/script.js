a70ad8368d5d7baff266824c7cfb6321bf946116
Msramirez93 <melany.salome.ramirez@gmail.com>
2021-04-24 15:38:17 -0300

    initial commit

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-document.querySelector('lastmod').textContent = document.lastModified;
+document.querySelector('#lastmod').textContent = document.lastModified;
 /*const date = new Date();
 document.querySelector("#Year").innerHTML = date.getFullYear();
  document.querySelector("#LastUpdate").innerHTML = document.lastModified;*/
