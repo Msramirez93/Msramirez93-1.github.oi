5a9ff633324b5e657f27e78a103e8833469dc212
Msramirez93 <melany.salome.ramirez@gmail.com>
2021-04-24 21:16:05 -0300

    initial commit

--- js/script.js
+++ js/script.js
@@ -1,3 +1,38 @@
-document.querySelector("#Year").innerHTML=document.lastModified;
-document.querySelector('#lastmod').textContent = document.lastModified;
+/*document.querySelector("#Year").innerHTML=document.lastModified;
+document.querySelector('#lastmod').textContent = document.lastModified;*/
 
+/*const todaysDate = new Date();
+document.querySelector(`#idName`).textContent = `${todaysDate.getFullYear()}`;*/
+const dayNames = [
+	`Sunday`,
+	`Monday`,
+	`Tuesday`,
+	`Wednesday`,
+	`Thursday`,
+	`Friday`,
+	`Saturday`
+  ]
+  
+  const monthNames = [
+	`January`,
+	`February`,
+	`March`,
+	`April`,
+	`May`,
+	`June`,
+	`July`,
+	`August`,
+	`September`,
+	`October`,
+	`November`,
+	`December`
+  ]
+  
+  const todaysDate = new Date();
+  const dayName = dayNames[todaysDate.getDay()];
+  const monthName = monthNames[todaysDate.getMonth()];
+  const currentDate = `${dayName}, ${todaysDate.getDate()} ${monthName}, ${todaysDate.getFullYear()}`;
+  
+  document.getElementById(`currentDate`).textContent = currentDate;
+  document.querySelector(`#rigthsYear`).textContent = `${todaysDate.getFullYear()}, `;
+  document.getElementById(`modifiedTime`).textContent = `${document.lastModified}`;
