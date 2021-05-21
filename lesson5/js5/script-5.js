36dd27c9542840db4579b5b257c08e25b06b2f93
Msramirez93 <melany.salome.ramirez@gmail.com>
2021-05-21 15:50:22 -0300

    initial commit

---
+++ lesson5/js5/script-5.js
@@ -0,0 +1,5 @@
+function toggleMenu() {
+    // console.log(document.getElementById("primaryNav").class);
+    document.getElementById("primaryNav").classList.toggle("hide");
+}
+document.querySelector('#lastmod').textContent = document.lastModified;
