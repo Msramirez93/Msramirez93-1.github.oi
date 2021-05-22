72c63a4d66ac33f7915a69d379682a1cd6cbbbd6
Msramirez93 <melany.salome.ramirez@gmail.com>
2021-05-22 17:17:07 -0300

    initial commit

--- lesson5/js5/script-5.js
+++ lesson5/js5/script-5.js
@@ -3,3 +3,18 @@ function toggleMenu() {
     document.getElementById("primaryNav").classList.toggle("hide");
 }
 document.querySelector('#lastmod').textContent = document.lastModified;
+// for current date in the footer
+/*const yearElement = document.querySelector("#date");*/
+/*yearElement.innerText = new Date().toLocaleDateString('en-GB', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' });*/
+
+
+// for the banner
+const banner = document.querySelector('.banner');
+const dayOfWeek = new Date().getDay();
+if (dayOfWeek === 5) {
+    banner.classList.add('visible');
+}
+
+
+
+
