aa5e37aa71f21c4c6c9b055f2f943e2c841ded9e
Msramirez93 <melany.salome.ramirez@gmail.com>
2021-05-31 08:23:58 -0300

    initial commit

--- lesson6/script-6/windchill.js
+++ lesson6/script-6/windchill.js
@@ -1,12 +1,12 @@
-let t = 47;
-let s = 5.2;
+let temp = 47;
+let speed = 5.2;
 let none = 'N/A';
 
-document.querySelector('.temperature').textContent = ` ${t} ${'°F'}`;
-document.querySelector('.speed').textContent = ` ${s} ${'mph'}`;
+document.querySelector('.temperature').textContent = ` ${temp} ${'°F'}`;
+document.querySelector('.speed').textContent = ` ${speed} ${'mph'}`;
 
-if (t <= 50 && s >= 3.0) {
-    let f = (35.74 + (0.6215 * t)) - (35.75 * (Math.pow(s, 0.16))) + (0.4275 * (t * (Math.pow(s, 0.16))));
+if (temp <= 50 && speed >= 3.0) {
+    let f = (35.74 + (0.6215 * temp)) - (35.75 * (Math.pow(speed, 0.16))) + (0.4275 * (temp * (Math.pow(speed, 0.16))));
     let result = f.toFixed(2);
     document.querySelector('.wChill').textContent = ` ${result} ${'°F'}`;
 } else {
