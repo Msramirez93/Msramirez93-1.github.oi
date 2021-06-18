385e3605d3b74872d34aadcbacd53f626856d0d1
Msramirez93 <melany.salome.ramirez@gmail.com>
2021-06-18 15:08:08 -0300

    initial commit

---
+++ lesson9/js9/prophets.js
@@ -0,0 +1,34 @@
+const requestURL = 'https://byui-cit230.github.io/lessons/lesson-09/data/latter-day-prophets.json';
+
+fetch(requestURL)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (jsonObject) {
+        // console.table(jsonObject);
+
+        const prophets = jsonObject['prophets'];
+        for (let i = 0; i < prophets.length; i++ ) {
+            let card = document.createElement('section');
+            let h2 = document.createElement('h2');
+            let image = document.createElement('img');
+            let birthdate = document.createElement('div');
+            let birthplace = document.createElement('div');
+
+            h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
+
+            birthdate.setAttribute('class', 'birth');
+            birthdate.textContent = 'Date of Birth: ' + prophets[i].birthdate;
+            birthplace.setAttribute('class', 'birthplace');
+            birthplace.textContent = 'Place of Birth: ' + prophets[i].birthplace;
+            image.setAttribute('src', prophets[i].imageurl);
+            image.setAttribute('alt', prophets[i].name + ' ' + prophets.lastname);
+
+            card.appendChild(h2);
+            card.appendChild(birthdate);
+            card.appendChild(birthplace);
+            card.appendChild(image);
+
+            document.querySelector('div.cards').appendChild(card);
+        }
+    });
